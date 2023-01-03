fde973c3241173aea722a85947ac1090bf14ce4b
woooyen <110808374+woooyen@users.noreply.github.com>
2023-01-03 11:41:44 +0900

    Update 현수막.py

--- Yen/현수막.py
+++ Yen/현수막.py
@@ -3,17 +3,14 @@
 import sys
 sys.setrecursionlimit(100000)
 
-
 n,m=list(map(int,input().split()))
 arr=[]
 for i in range(n):
   arr.append(list(map(int,input().split())))
 
-
 dx=[-1,1,0,0,-1,-1,1,1]
 dy=[0,0,-1,1,-1,1,-1,1]
 count=0
-
 
 def dfs(x,y) :
   arr[x][y]=0
@@ -23,14 +20,12 @@ def dfs(x,y) :
     if 0<= nx < n and 0 <= ny < m and arr[nx][ny]==1:
       dfs(nx,ny)
 
-
 for i in range(n):
   for j in range(m):
     if arr[i][j]==1 :
       dfs(i,j)
       count +=1
-
-
+      
 print(count)
 
 
@@ -45,12 +40,10 @@ arr=[]
 for i in range(n):
   arr.append(list(map(int,input().split())))
 
-
 dx=[-1,1,0,0,-1,-1,1,1]
 dy=[0,0,-1,1,-1,1,-1,1]
 count=0
 q=deque()
-
 
 def bfs(x,y) :
   q.append((x,y))
@@ -63,7 +56,6 @@ def bfs(x,y) :
       if 0 <= nx < n and 0 <= ny < m and arr[nx][ny]==1 :
         q.append((nx,ny))
         arr[nx][ny]=0
-
 
 
 for i in range(n):
