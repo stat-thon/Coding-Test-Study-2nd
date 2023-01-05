f327977f0a55982714e20aa9a2faa068c064868c
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-01-05 18:18:22 +0900

    Create youare1fromnow.py

---
+++ Dawny/BOJ/02ND/youare1fromnow.py
@@ -0,0 +1,14 @@
+# input
+x = int(input())
+
+# value
+d = [0] * (x+1)
+
+# DP
+for i in range(2, x+1):
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3]+1)
+print(d[x])
