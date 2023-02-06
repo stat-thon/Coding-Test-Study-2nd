56f7cb4f89857a19768ea60c5f903622c71fc45f
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-02-06 09:20:28 +0900

    Update coin1.py

--- Dawny/BOJ/06TH/coin1.py
+++ Dawny/BOJ/06TH/coin1.py
@@ -1,10 +1,9 @@
 # input
 n, k, *coin = map(int, open(0).read().split())
 
-method = [1 if i == 0 else 0 for i in range(k+1)]
+method = [1 if i % coin[0] == 0 else 0 for i in range(k+1)]
 
-for j in coin:
+for j in coin[1:]:
     for k in range(j, k+1):
         method[k] += method[k-j]
-
 print(method[-1])
