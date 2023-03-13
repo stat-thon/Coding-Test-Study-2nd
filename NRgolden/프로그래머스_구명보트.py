1e67b35d5e719c70e7ee329d13316e1db40ee504
Narin <62434201+nrgolden16@users.noreply.github.com>
2023-03-13 14:01:59 +0900

    Create 프로그래머스_구명보트.py

---
+++ NRgolden/프로그래머스_구명보트.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(people, limit):
+    q = deque(sorted(people))
+    answer=0
+    while q:
+        
+        if len(q)==1:
+            answer+=1
+            break
+        if q[0] + q[-1]<= limit:
+            q.pop()
+            q.popleft()
+        else:
+            q.pop()
+        answer+=1
+    return answer
