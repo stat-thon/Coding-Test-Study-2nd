d88223ab3258e735e502d6abb0ac281d8dae11e1
woooyen <110808374+woooyen@users.noreply.github.com>
2023-03-13 14:30:53 +0900

    Update 귤고르기.py

--- Yen/프로그래머스/귤고르기.py
+++ Yen/프로그래머스/귤고르기.py
@@ -5,9 +5,7 @@ def solution(k, tangerine):
         box[i]=0
     for i in tangerine :
         box[i] +=1
-    print(box)
     box=sorted(box.items(), key=lambda x: x[1],reverse=True)
-    print(box)
     for i, j in enumerate(box):
         c += j[1]
         if c >= k :
