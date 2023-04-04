3aff482fddbc976198fbd327ed91d9d0c4a692e5
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-04-04 17:34:45 +0900

    Create Febatta.py

---
+++ Dawny/BOJ/15TH/Febatta.py
@@ -0,0 +1,17 @@
+# input
+n = input()
+stack = []
+cnt = 0
+
+for i in range(len(n)):
+    if n[i] == "(":
+        stack.append('(')
+    else:
+        if n[i-1] == "(":
+            stack.pop()
+            cnt += len(stack)
+        else:
+            stack.pop()
+            cnt += 1
+
+print(cnt)
