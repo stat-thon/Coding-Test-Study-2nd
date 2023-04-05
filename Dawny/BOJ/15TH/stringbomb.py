fa4bdc1a78dcd274906bcbc93709474c8f188995
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-04-05 15:45:42 +0900

    Create stringbomb.py

---
+++ Dawny/BOJ/15TH/stringbomb.py
@@ -0,0 +1,17 @@
+# module
+import sys
+
+# input
+input = sys.stdin.readline
+s = input().rstrip()
+bomb = input().rstrip()
+stack = []
+n = len(bomb)
+
+for i in s:
+    stack.append(i)
+    if ''.join(stack[-n:]) == bomb:
+        for _ in range(n):
+            stack.pop()
+
+print(''.join(stack))
