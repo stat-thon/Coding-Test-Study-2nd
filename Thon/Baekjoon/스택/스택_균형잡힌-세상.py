109f39e65852205d669c35b2c76536ae0a4c1402
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2023-04-13 14:32:29 +0900

    update py

--- Thon/Baekjoon/스택/스택_균형잡힌-세상.py
+++ Thon/Baekjoon/스택/스택_균형잡힌-세상.py
@@ -27,3 +27,37 @@ while True:
         print('no')
     else:
         print('yes')
+
+
+# 다시 내 풀이
+def is_parent(word):
+
+    pdict = {')': '(', ']': '['}
+    stack = []
+
+    for char in word:
+        if char not in pdict:
+            stack.append(char)
+
+        elif not stack or pdict[char] != stack.pop():
+            return 'no'
+    
+    if len(stack) == 0:
+        return 'yes'
+    
+    return 'no'
+
+while True:
+    
+    i = input()
+    
+    if i == '.':
+        break
+        
+    one = []
+    for s in i:
+        if s == '.':
+            print(is_parent(one))
+
+        elif s in ('(', ')', '[',']'):
+            one.append(s)
