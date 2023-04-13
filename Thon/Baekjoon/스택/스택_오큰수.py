b14ce85a2652dc9d304d3e62abe30c9f9b976fac
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2023-04-13 14:47:31 +0900

    upload py

--- Thon/Baekjoon/스택/스택_오큰수.py
+++ Thon/Baekjoon/스택/스택_오큰수.py
@@ -1,71 +1,12 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "5 7 7 -1 "
-     ]
-    }
-   ],
-   "source": [
-    "n = int(input())\n",
-    "arr = list(map(int, input().split()))\n",
-    "result = [-1] * n\n",
-    "stack = []\n",
-    "\n",
-    "for i in range(n):\n",
-    "    while stack and arr[stack[-1]] < arr[i]:\n",
-    "        result[stack.pop()] = arr[i]\n",
-    "    \n",
-    "    stack.append(i)\n",
-    "\n",
-    "print(*result)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "5 7 7 -1\n"
-     ]
-    }
-   ],
-   "source": [
-    "print(*result)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "thon",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+n = int(input())
+arr = list(map(int, input().split()))
+result = [-1] * n
+stack = []
+
+for i in range(n):
+    while stack and arr[stack[-1]] < arr[i]:
+        result[stack.pop()] = arr[i]
+    
+    stack.append(i)
+
+print(*result)
