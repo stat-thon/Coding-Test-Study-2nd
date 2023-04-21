0801b8d09389c9221c0bb40a048f2a95e7c54025
Da-Woon Kim <76687996+Daw-ny@users.noreply.github.com>
2023-04-21 18:16:32 +0900

    Update persgunezreal.py

--- Dawny/Programmers/18TH/persgunezreal.py
+++ Dawny/Programmers/18TH/persgunezreal.py
@@ -1,5 +1,5 @@
 def solution(sequence):
-    dp = [0] * len(sequence)
+    dp = [0] * (len(sequence)+1)
     for i in range(len(sequence)):
         dp[i] = dp[i-1] + sequence[i] * (-1) ** (i)
     return max(dp) - min(dp)
